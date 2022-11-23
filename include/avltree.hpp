4128f21fa9a4fef34fafa447e99326443c651959
frank2 <frank2@dc949.org>
2022-11-23 03:13:43 -0800

    missing semicolon

--- include/avltree.hpp
+++ include/avltree.hpp
@@ -1025,7 +1025,7 @@ namespace exception {
          if (other._root == nullptr) { return; }
 
          std::vector<ConstSharedNode> visiting = { other._root };
-         std::vector<SharedNode> added = { this->copy_node(other._root) }
+         std::vector<SharedNode> added = { this->copy_node(other._root) };
          std::size_t parent_index = 0;
 
          this->_root = added.front();
